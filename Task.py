fceea0ac544c155af4024550bece7899626e0161
Eylon Iyov <eylon.iyov@pos.runi.ac.il~>
2025-03-20 16:43:59 +0200

    my second commit

---
+++ Task.py
@@ -0,0 +1,34 @@
+class Task:
+
+
+    def __init__(self, title, description, priority: int):
+        self.title = title
+        self.description = description
+        self.done = False
+        self.priority = priority
+
+    def __str__(self):
+        return f"""
+                Task Name: {self.title}
+                Description: {self.description}
+                Priority: {self.priority}
+                Is it done: {self.done}
+                """
+
+    def __repr__(self):
+        return f"Task Name: {self.title}, Description: {self.description}, Priority: {self.priority}"
+
+    def mark_done(self):
+        self.done = True
+
+    def mark_priority(self, priority: int):
+        self.priority = priority;
+
+    def getTitle(self):
+        return self.title
+
+    def getDescription(self):
+        return self.description
+
+    def getPriority(self):
+        return self.priority
---
+++ Todo_app.py
@@ -0,0 +1,100 @@
+from time import sleep
+
+from Task import Task
+
+
+def add(taskList: list):
+    title = input("What is the task title? \n").strip()
+    Description = input("Please enter the description of the task \n").strip()
+    priority = input("Please enter the priority of the task \n")
+    taskList.append(Task(title, Description, priority))
+    return
+
+def removeTask(taskList: list):
+    title = input("What is the task name you would like to remove? \n").strip()
+    if title not in list(task.getTitle() for task in taskList):
+        print("Invalid title \n")
+        return
+    for task in taskList:
+        if task.getTitle() == title:
+            print(f"The task: {task.getTitle()} has been successfuly removed \n")
+            taskList.remove(task)
+            return
+
+def removeAllTasks(taskList: list):
+    for task in taskList:
+        taskList.remove(task)
+        return
+
+def updateTask(taskList: list):
+    title = input("What is the title of the task you would like to update? \n").strip()
+    if title not in list(task.getTitle() for task in taskList):
+        print("Invalid title\n")
+        return
+    taskFields = ["title", "description", "priority"]
+    updatedField = input("What is the field you would like to update? \n").strip()
+    if updatedField not in taskFields:
+        print("Invalid field \n")
+        return
+    for task in taskList:
+        if task.getTitle() == title:
+            changeField(task, updatedField)
+            print(f"updated field {updatedField} of task {task.title} \n")
+            sleep(3)
+            print("Done")
+
+
+def markTask(taskList: list):
+    title = input("What is the title of the task you would like to mark as finished? \n")
+    if title not in list(task.getTitle() for task in taskList):
+        print("Invalid title \n")
+        return
+    for task in taskList:
+        if task.getTitle() == title:
+            task.mark_done()
+
+def changeField(task, field):
+    if field == "description":
+        task.description = input("What is the new description of the task? \n")
+    if field == "priority":
+        task.priority = input("What is the new priority of the task? \n")
+    if field == "title":
+        task.title = input("What is the new title of the task? \n")
+
+def viewTasks(taskList: list):
+    for task in taskList:
+        print(task)
+
+def sortTasks(taskList: list):
+    list.sort(key=lambda task: task.priority, reverse=True)
+
+def menu(taskList: list):
+    while True:
+        action = input("""What operation would you like to perform? \n 1. add \n 2. remove\n 3. update\n 4. mark\n 5. view\n 6. exit \n""")
+        action = action.lower().strip()
+        actions = ["add", "remove", "update", "mark", "view", "exit"]
+        if action not in actions:
+            print("Invalid action")
+            sleep(2)
+            continue
+        else:
+            if action == "add" or action == "1":
+                add(taskList)
+            elif action == "remove" or action == "2":
+                removeTask(taskList)
+            elif action == "update" or action == "3":
+                updateTask(taskList)
+            elif action == "mark" or action == "4":
+                markTask(taskList)
+            elif action == "view" or action == "5":
+                viewTasks(taskList)
+            elif action == "exit"or action == "6":
+                print("Goodbye")
+                break
+
+def main():
+    taskList = []
+    menu(taskList)
+
+if __name__ == "__main__":
+    main()
